2bf28097afa6549c047a99de0beb868329ac3da2
isarmendes <isadoramendes@aluno.senai.br>
2023-11-09 15:09:47 -0300

    feat: server config

--- src/server.js
+++ src/server.js
@@ -6,3 +6,15 @@ config(); // Lê o arquivo .env
 const port = process.env.PORT || 5000;//Defini a porta que será usada 
 
 const app = express(); //Criar o servidor 
+app.use(express.json());// Permite que o servidor interprete o formato JSON
+
+app.get("/", (req, res) => {
+    //Rota inicial
+    res.status(200).json({massege:"Hello, World!"});
+
+})
+app.listen(port,()=>{
+    //Inicia o servidor
+    console.log(`⚡server started 
+    on http://localhost:${port}`);
+});
